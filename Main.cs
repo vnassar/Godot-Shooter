f78e6fff74f2dd48a87e3d72289024a506548a8c
Victor <vctrnssr@gmail.com>
2026-06-10 19:40:31 -0400

    trenchbroom setup

---
+++ Main.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class Main : Node3D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("Godot C# is working :D");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
